43d7d7d436c85d70f7d85f29dbb23e458a0c4b23
RainStorMtj <145570462+RainStorMtj@users.noreply.github.com>
2024-11-23 22:05:06 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def devide(first, second):
+    if second == 0:
+        return "Ошибка"
+    else:
+        dev_result = first / second
+        return dev_result
---
+++ true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+def devide(first, second):
+    if second == 0:
+        return inf
+    else:
+        dev_result = int(first) / int(second)
+        return dev_result
